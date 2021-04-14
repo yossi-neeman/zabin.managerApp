12ef9cf9710432a3d74b19643d06f7ed48fc8983
Yossi Neeman <Yossi.Neeman@nice.com>
2021-04-14 13:01:10 +0300

    update authToken

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get("/about", (req,res)=> {
 })
 
 var accountSid = 'AC531e5519183aa0bc2571f62e17495da8'; // Your Account SID from www.twilio.com/console
-  var authToken = 'f08f11a93b9ccbc551a0c5b5706e6164';   // Your Auth Token from www.twilio.com/console
+  var authToken = '962e729a6a42a973891ae9833196222c';   // Your Auth Token from www.twilio.com/console
   
   var twilio = require('twilio');
   var twilioClient = new twilio(accountSid, authToken);
