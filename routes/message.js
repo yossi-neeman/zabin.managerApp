adb0fa4df4d208b095755ebe8cc023d1c7db9147
yossi neeman <yossi.neeman.w@gmail.com>
2021-04-20 22:16:46 +0300

    fix json representation

--- routes/message.js
+++ routes/message.js
@@ -18,7 +18,7 @@ router.post('/', function(req, res, next) {
         body: 'message was: ' + '*' + req.body.Body + '*',
         from: 'whatsapp:+14155238886',
         to: req.body.From,
-        MediaUrl0="https://zabin-manager.s3.us-east-2.amazonaws.com/images/milk3precentTara.jpg"
+        MediaUrl0:"https://zabin-manager.s3.us-east-2.amazonaws.com/images/milk3precentTara.jpg"
       })
       .then((message) => console.log(message.sid))
       .catch(err => console.log(err))
